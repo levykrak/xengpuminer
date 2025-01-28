17e694a03b194584bc47a0d6a3e7294e7be4973d
levykrak <44068840+levykrak@users.noreply.github.com>
2025-01-28 13:12:31 +0100

    Update withdrawer.sh

--- withdrawer.sh
+++ withdrawer.sh
@@ -9,5 +9,6 @@ $SOLANA_BIN withdraw-from-vote-account $CONFIG_DIR/vote.json $CONFIG_DIR/id.json
 $SOLANA_BIN deactivate-stake $CONFIG_DIR/stake2.json -k $CONFIG_DIR/id.json
 sleep 200
 $SOLANA_BIN withdraw-stake $CONFIG_DIR/stake2.json $CONFIG_DIR/id.json ALL -k $CONFIG_DIR/id.json
+sleep 5
 $SOLANA_BIN create-stake-account $CONFIG_DIR/stake2.json ALL -k $CONFIG_DIR/id.json --fee-payer $CONFIG_DIR/identity.json
 $SOLANA_BIN delegate-stake $CONFIG_DIR/stake2.json $CONFIG_DIR/vote.json -k $CONFIG_DIR/id.json --fee-payer $CONFIG_DIR/identity.json
