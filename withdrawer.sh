ba2d4864a024eb86ecd0d066105fee0944ce6416
levykrak <44068840+levykrak@users.noreply.github.com>
2025-03-05 13:16:34 +0100

    Update withdrawer.sh
    zmiana aby na koncie id.json zostawalo 0.1 SOL pod pingera

--- withdrawer.sh
+++ withdrawer.sh
@@ -8,6 +8,12 @@ SOLANA_BIN="/usr/bin/solana"  # Zmień na odpowiednią ścieżkę do pliku wykon
 $SOLANA_BIN withdraw-from-vote-account $CONFIG_DIR/vote.json $CONFIG_DIR/id.json ALL --fee-payer $CONFIG_DIR/identity.json -k $CONFIG_DIR/id.json
 $SOLANA_BIN deactivate-stake $CONFIG_DIR/stake2.json -k $CONFIG_DIR/id.json
 sleep 200
+
 $SOLANA_BIN withdraw-stake $CONFIG_DIR/stake2.json $CONFIG_DIR/id.json ALL -k $CONFIG_DIR/id.json
-$SOLANA_BIN create-stake-account $CONFIG_DIR/stake2.json ALL -k $CONFIG_DIR/id.json
+
+#obliczamy stake jaki mozemy wrzucic do stake -0.1
+amount=$(solana balance $CONFIG_DIR/id.json | awk '{print $1}')
+amount=$(echo "$amount - 0.1" | bc)
+
+$SOLANA_BIN create-stake-account $CONFIG_DIR/stake2.json $amount -k $CONFIG_DIR/id.json
 $SOLANA_BIN delegate-stake $CONFIG_DIR/stake2.json $CONFIG_DIR/vote.json -k $CONFIG_DIR/id.json --fee-payer $CONFIG_DIR/identity.json
