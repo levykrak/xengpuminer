6262e1b81596eca1cac00c2258dbf758129e763b
levykrak <44068840+levykrak@users.noreply.github.com>
2025-03-12 10:05:14 +0100

    Update withdrawer.sh
    zostawiamy na id.json aż 1 SOL

--- withdrawer.sh
+++ withdrawer.sh
@@ -13,7 +13,7 @@ $SOLANA_BIN withdraw-stake $CONFIG_DIR/stake2.json $CONFIG_DIR/id.json ALL -k $C
 
 #obliczamy stake jaki mozemy wrzucic do stake -0.1
 amount=$(solana balance $CONFIG_DIR/id.json | awk '{print $1}')
-amount=$(echo "$amount - 0.1" | bc)
+amount=$(echo "$amount - 1" | bc)
 
 $SOLANA_BIN create-stake-account $CONFIG_DIR/stake2.json $amount -k $CONFIG_DIR/id.json
 $SOLANA_BIN delegate-stake $CONFIG_DIR/stake2.json $CONFIG_DIR/vote.json -k $CONFIG_DIR/id.json --fee-payer $CONFIG_DIR/identity.json
